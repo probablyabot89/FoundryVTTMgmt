491fc08ea6f4a7a3a7ae5f6c82644e0cb03845df
probablyabot89 <64726440+probablyabot89@users.noreply.github.com>
2023-11-09 15:32:47 +0000

    Update foundry API.psm1
    added API features

--- foundry API.psm1
+++ foundry API.psm1
@@ -1,9 +1,11 @@
+# FoundryAPI.psm1
+
 <#
 .SYNOPSIS
 A module for interacting with Foundry VTT API.
 
 .DESCRIPTION
-This module provides functions to authenticate with Foundry VTT API, discover and select API endpoints, and send data to the selected endpoint.
+This module provides functions to authenticate with Foundry VTT API, discover and select API endpoints, and send data to the selected endpoint. It also includes fun features for managing games, players, chat, dice rolling, entity spawning, and simulating events.
 
 .NOTES
 File Name      : FoundryAPI.psm1
@@ -30,7 +32,7 @@ function Connect-FoundryAPI {
 }
 
 # Function to discover Foundry VTT API endpoints
-function Discover-FoundryAPIEndpoints {
+function Get-FoundryAPIEndpoints {
     param (
         $Connection
     )
@@ -60,7 +62,7 @@ function Select-FoundryAPIEndpoint {
 }
 
 # Function to send data to a selected API endpoint
-function Send-FoundryAPIData {
+function Invoke-FoundryAPICall {
     param (
         $Connection,
         [string]$Endpoint,
@@ -85,9 +87,7 @@ function New-FoundryGame {
         system = $System
     }
 
-    $response = Invoke-RestMethod -Uri "$($Connection.ServerUrl)/api/games" -Method Post -Headers $Connection.Headers -Body ($body | ConvertTo-Json)
-
-    return $response
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint 'games' -Data $body
 }
 
 # Function to get game details from Foundry VTT
@@ -97,22 +97,18 @@ function Get-FoundryGameDetails {
         [string]$GameId
     )
 
-    $response = Invoke-RestMethod -Uri "$($Connection.ServerUrl)/api/games/$GameId" -Method Get -Headers $Connection.Headers
-
-    return $response
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId" -Data @{}
 }
 
 # Function to update game settings in Foundry VTT
-function Update-FoundryGameSettings {
+function Set-FoundryGameSettings {
     param (
         $Connection,
         [string]$GameId,
         [hashtable]$Settings
     )
 
-    $response = Invoke-RestMethod -Uri "$($Connection.ServerUrl)/api/games/$GameId/settings" -Method Put -Headers $Connection.Headers -Body ($Settings | ConvertTo-Json)
-
-    return $response
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/settings" -Data $Settings
 }
 
 # Function to start or stop Foundry VTT server
@@ -123,9 +119,152 @@ function Toggle-FoundryServer {
         [bool]$Start
     )
 
-    $action = if ($Start) { "start" } else { "stop" }
+    $action = if ($Start) { 'start' } else { 'stop' }
 
-    $response = Invoke-RestMethod -Uri "$($Connection.ServerUrl)/api/games/$GameId/$action" -Method Post -Headers $Connection.Headers
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/$action" -Data @{}
+}
 
-    return $response
+# Function to get player information
+function Get-FoundryPlayers {
+    param (
+        $Connection,
+        [string]$GameId
+    )
+
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/players" -Data @{}
+}
+
+# Function to send in-game chat messages
+function Send-FoundryChatMessage {
+    param (
+        $Connection,
+        [string]$GameId,
+        [string]$Message
+    )
+
+    $body = @{
+        content = $Message
+    }
+
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/chat" -Data $body
+}
+
+# Function to roll virtual dice
+function Roll-FoundryDice {
+    param (
+        $Connection,
+        [string]$GameId,
+        [string]$Formula
+    )
+
+    $body = @{
+        formula = $Formula
+    }
+
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/roll" -Data $body
+}
+
+# Function to spawn a creature or item in the game
+function Spawn-FoundryEntity {
+    param (
+        $Connection,
+        [string]$GameId,
+        [string]$EntityName,
+        [int]$X,
+        [int]$Y
+    )
+
+    $body = @{
+        name = $EntityName
+        x    = $X
+        y    = $Y
+    }
+
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/entities/spawn" -Data $body
+}
+
+# Function to simulate a random event in the game
+function Simulate-FoundryEvent {
+    param (
+        $Connection,
+        [string]$GameId,
+        [string]$EventType
+    )
+
+    $body = @{
+        type = $EventType
+    }
+
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/events/simulate" -Data $body
+}
+
+# Function to get list of available entities in the game
+function Get-FoundryEntities {
+    param (
+        $Connection,
+        [string]$GameId
+    )
+
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/entities" -Data @{}
+}
+
+# Function to move an entity to a new position
+function Move-FoundryEntity {
+    param (
+        $Connection,
+        [string]$GameId,
+        [string]$EntityId,
+        [int]$X,
+        [int]$Y
+    )
+
+    $body = @{
+        x = $X
+        y = $Y
+    }
+
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/entities/$EntityId/move" -Data $body
+}
+
+# Function to delete an entity from the game
+function Remove-FoundryEntity {
+    param (
+        $Connection,
+        [string]$GameId,
+        [string]$EntityId
+    )
+
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/entities/$EntityId/remove" -Data @{}
+}
+
+# Function to simulate a weather change in the game
+function Simulate-FoundryWeather {
+    param (
+        $Connection,
+        [string]$GameId,
+        [string]$WeatherType
+    )
+
+    $body = @{
+        type = $WeatherType
+    }
+
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/weather/simulate" -Data $body
+}
+
+# Function to create a custom game event
+function Trigger-FoundryCustomEvent {
+    param (
+        $Connection,
+        [string]$GameId,
+        [string]$EventName,
+        [string]$EventData
+    )
+
+    $body = @{
+        name = $EventName
+        data = $EventData
+    }
+
+    return Invoke-FoundryAPICall -Connection $Connection -Endpoint "games/$GameId/events/trigger" -Data $body
 }
