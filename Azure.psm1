138dbb899b516e91fcaf93b760682676734d8bf4
probablyabot89 <64726440+probablyabot89@users.noreply.github.com>
2023-11-09 15:10:16 +0000

    Update Azure.psm1
    added documentation to functions

--- Azure.psm1
+++ Azure.psm1
@@ -1,6 +1,18 @@
 # Import required modules
 Import-Module Az
 
+<#
+.SYNOPSIS
+A module for managing Foundry VTT instances with Azure resources.
+
+.DESCRIPTION
+This module provides functions to initialize Azure resources, stop Foundry servers, remove Azure resources, monitor Azure costs, and get key VM stats.
+
+.NOTES
+File Name      : FoundryAzure.psm1
+Prerequisite   : PowerShell v5.1 or later
+#>
+
 # Function to initialize Azure resources for Foundry
 function Initialize-FoundryAzure {
     param (
@@ -12,28 +24,13 @@ function Initialize-FoundryAzure {
         [string]$OpenPorts = "30000"
     )
 
-    # Create a new resource group
     az group create --name $ResourceGroup --location $Location
-
-    # Create a public IP address
     az network public-ip create --resource-group $ResourceGroup --name $PublicIPAddress --sku Standard --allocation-method Dynamic
-
-    # Create a network security group
     az network nsg create --resource-group $ResourceGroup --name "foundry-nsg" --location $Location
-
-    # Create a rule in the network security group to allow traffic on specified ports
     az network nsg rule create --resource-group $ResourceGroup --nsg-name "foundry-nsg" --name "AllowFoundry" --priority 100 --source-address-prefix Internet --source-port-range "*" --destination-address-prefix "*" --destination-port-range $OpenPorts --access Allow
-
-    # Create a virtual network
     az network vnet create --resource-group $ResourceGroup --name "foundry-vnet" --address-prefixes "10.0.0.0/16" --location $Location
-
-    # Create a subnet and associate it with the virtual network
     az network vnet subnet create --resource-group $ResourceGroup --vnet-name "foundry-vnet" --name "foundry-subnet" --address-prefixes "10.0.0.0/24"
-
-    # Create a network interface and associate it with the subnet
     az network nic create --resource-group $ResourceGroup --name "foundry-nic" --vnet-name "foundry-vnet" --subnet "foundry-subnet"
-
-    # Create a virtual machine
     az vm create --resource-group $ResourceGroup --name $VMName --location $Location --size $VMSize --image UbuntuLTS --admin-username azureuser --generate-ssh-keys --nics "foundry-nic" --boot-diagnostics-storage "foundrydiagstorage"
 }
 
@@ -44,7 +41,6 @@ function Stop-FoundryServer {
         [string]$VMName
     )
 
-    # Stop Foundry server (e.g., Azure CLI commands)
     az vm deallocate --resource-group $ResourceGroup --name $VMName
 }
 
@@ -54,7 +50,6 @@ function Remove-FoundryAzure {
         [string]$ResourceGroup
     )
 
-    # Remove Azure resources
     az group delete --name $ResourceGroup --yes --no-wait
 }
 
@@ -64,22 +59,14 @@ function Monitor-AzureCosts {
         [string]$ResourceGroup
     )
 
-    # Calculate start and end date for the rolling last 4 months
     $endDate = Get-Date
     $startDate = (Get-Date).AddMonths(-4)
-
-    # Format dates as required by Azure CLI
     $startDateFormatted = $startDate.ToString("yyyy-MM-dd")
     $endDateFormatted = $endDate.ToString("yyyy-MM-dd")
 
-    # Monitor Azure costs
     Write-Host "Monitoring Azure costs for resources in group $ResourceGroup..."
 
-    # Retrieve cost details using Azure Cost Management APIs
-    $costDetails = az consumption usage list --start-date $startDateFormatted --end-date $endDateFormatted --query "[?contains('instanceName', '$ResourceGroup')].{Name: instanceName, Cost: preTaxCost}" --output table
-    
-    Write-Host "Cost Details for Resource Group: $ResourceGroup"
-    Write-Host $costDetails
+    az consumption usage list --start-date $startDateFormatted --end-date $endDateFormatted --query "[?contains('instanceName', '$ResourceGroup')].{Name: instanceName, Cost: preTaxCost}" --output table
 }
 
 # Function to get key VM stats
@@ -89,21 +76,7 @@ function Get-FoundryVMStats {
         [string]$VMName
     )
 
-    # Get CPU usage
-    $cpuUsage = az monitor metrics list --resource $VMName --resource-group $ResourceGroup --metric "Percentage CPU" --output json | ConvertFrom-Json
-
-    # Get memory usage
-    $memoryUsage = az monitor metrics list --resource $VMName --resource-group $ResourceGroup --metric "Available Memory Bytes" --output json | ConvertFrom-Json
-
-    # Get disk space usage
-    $diskUsage = az monitor metrics list --resource $VMName --resource-group $ResourceGroup --metric "Disk Space Used" --output json | ConvertFrom-Json
-
-    # Output PowerShell objects
-    [PSCustomObject]@{
-        VMName       = $VMName
-        ResourceGroup = $ResourceGroup
-        CPUUsage     = $cpuUsage
-        MemoryUsage  = $memoryUsage
-        DiskUsage    = $diskUsage
-    }
+    az monitor metrics list --resource $VMName --resource-group $ResourceGroup --metric "Percentage CPU" --output json | ConvertFrom-Json
+    az monitor metrics list --resource $VMName --resource-group $ResourceGroup --metric "Available Memory Bytes" --output json | ConvertFrom-Json
+    az monitor metrics list --resource $VMName --resource-group $ResourceGroup --metric "Disk Space Used" --output json | ConvertFrom-Json
 }
